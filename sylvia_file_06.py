eac1ebb00ce27d8329a05be5be807676b2a6bc41
dsylvia06 <dsylvianiveditha03@gmail.com>
2026-04-09 12:23:37 +0530

    added the function with parameters and printed the result

--- sylvia_file_06.py
+++ sylvia_file_06.py
@@ -1,3 +1,6 @@
-print("hello world")
-print("this is sylvia")
-print("this is for the second branch")
+print("this is an addition function")
+int a , b 
+
+def add(a,b):
+    return a + b   
+print("the sum of a and b is", add(5, 10))
